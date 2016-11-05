323ca381c7e8207341031710f7975b3302b04644
Antony Gabarron <antony-ag@hotmail.com>
2016-11-05 17:02:44 -0200

    Activity Detalhes

---
+++ app/src/main/java/br/com/fatecpg/mygrades/Detalhes.java
@@ -0,0 +1,13 @@
+package br.com.fatecpg.mygrades;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Detalhes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhes);
+    }
+}
