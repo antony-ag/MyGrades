708b50ed462ab9a529f56650145d1f3a21f02a2a
Antony Gabarron <antony-ag@hotmail.com>
2016-11-05 22:07:28 -0200

    Comunicacao entre activitys | Teclado | XML | metodos | comentarios

--- app/src/main/java/br/com/fatecpg/mygrades/Detalhes.java
+++ app/src/main/java/br/com/fatecpg/mygrades/Detalhes.java
@@ -1,13 +1,71 @@
 package br.com.fatecpg.mygrades;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class Detalhes extends AppCompatActivity {
+    private ArrayList<String> disciplinas = new  ArrayList<>();
+    private int idDisc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalhes);
+
+        //Recupera os dados passados pela main
+        Intent main = getIntent();
+        int idDisc = main.getIntExtra("idDisc", 0); //Este é o índice do array, corresponde a disciplina clicada no main activity
+        disciplinas = main.getStringArrayListExtra("disciplinas"); //Dados do ListView
+
+
+        Toast.makeText(
+            getApplicationContext()
+            , "Você escolheu a opção " + disciplinas.get(idDisc)
+            , Toast.LENGTH_SHORT
+        ).show();
+
     }
+
+    public void excluir(View view){
+
+        Toast.makeText(
+            getApplicationContext()
+            , "Disciplina " + disciplinas.get(idDisc) + " removida!"
+            , Toast.LENGTH_SHORT
+        ).show();
+        disciplinas.remove(idDisc);
+
+        /*
+        +++++++++++++++++++++++++++++++++++++++++++
+
+        EXCLUIR O ARQUIVO DE DISCIPLINA
+
+        +++++++++++++++++++++++++++++++++++++++++++
+        */
+
+        finish();
+    }
+
+    public void gravar(){
+        Toast.makeText(
+                getApplicationContext()
+                , "GRAVAR AS NOTAS NO ARQUIVO."
+                , Toast.LENGTH_SHORT
+        ).show();
+
+        /*
+        +++++++++++++++++++++++++++++++++++++++++++
+
+        GRAVAR OS DADOS NO ARQUIVO DE DISCIPLINA
+
+        +++++++++++++++++++++++++++++++++++++++++++
+        */
+
+    }
+
 }
--- app/src/main/java/br/com/fatecpg/mygrades/MainActivity.java
+++ app/src/main/java/br/com/fatecpg/mygrades/MainActivity.java
@@ -1,10 +1,14 @@
 package br.com.fatecpg.mygrades;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -12,15 +16,85 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private ArrayList<String> disciplinas = new  ArrayList<>();
+    private ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        for(int i = 1; i<=10; i++){
+        list = (ListView)findViewById(R.id.optionsListView);
+        LerDisciplinas();
+        RefreshList();
+        ListenerList();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        LerDisciplinas();
+        RefreshList();
+
+    }
+
+    public EditText getEditText(int id){
+        EditText e = (EditText) findViewById(id);
+        return e;
+    }
+
+    public void inserir(View view){
+        String newDisc;
+
+        hideKeyboard(view);
+
+        if (!(getEditText(R.id.editNewDisc).getText().toString().length() == 0)) {
+
+            newDisc = getEditText(R.id.editNewDisc).getText().toString();
+
+            disciplinas.add(newDisc);
+
+            /*
+            +++++++++++++++++++++++++++++++++++++++++++
+
+            CRIAR O NOVO ARQUIVO DE DISCIPLINA
+
+            +++++++++++++++++++++++++++++++++++++++++++
+            */
+
+            RefreshList();
+
+            Toast.makeText(getApplicationContext(),"Adicionado " + newDisc,Toast.LENGTH_LONG).show();
+
+
+        }
+
+    }
+
+    /**
+     * Adiciona ao array o nome das disciplinas guardadas nos arquivos
+     */
+    private void LerDisciplinas(){
+        disciplinas.clear();
+
+        /*
+        +++++++++++++++++++++++++++++++++++++++++++
+
+        LER TODOS OS ARQUIVOS DE DISCIPLINA E ADICIONAR NO ARRAY
+
+        +++++++++++++++++++++++++++++++++++++++++++
+        */
+
+        for(int i = 1; i<=3; i++){
             disciplinas.add("Opção " + i);
         }
+    }
+
+    /**
+     * Atualiza o Listview
+     */
+    private void RefreshList(){
 
         ArrayAdapter<String> aa = new ArrayAdapter<String>(
                 this
@@ -28,20 +102,40 @@ public class MainActivity extends AppCompatActivity {
                 , disciplinas
         );
 
-        ListView list = (ListView)findViewById(R.id.optionsListView);
         list.setAdapter(aa);
+    }
+
+    /**
+     * Monitora o ListView
+     */
+    private void ListenerList(){
 
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
-                Toast.makeText(
-                        getApplicationContext()
-                        , "Você escolheu a opção " + (i+1)
-                        , Toast.LENGTH_SHORT
-                ).show();
+                CallDetalhes(disciplinas, i);
             }
         });
+    }
 
+    /**
+     * Chama a activity detalhes.
+     * @param array Disciplinas
+     * @param index Índice
+     */
+    private void CallDetalhes(ArrayList<String> array, int index){
+        Intent detalhes = new Intent(getApplicationContext(), Detalhes.class);
+        detalhes.putExtra("idDisc", index);
+        detalhes.putExtra("disciplinas", array);
+        startActivity(detalhes);
+    }
 
+    /**
+     * Oculta o teclado
+     * @param view
+     */
+    private void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager) view.getContext().getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
